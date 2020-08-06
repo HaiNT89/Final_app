f8e1a88e1bba8f3ae7fba7683729220efa6d7fc7
Thanh Hai <thanhhai691999@gmail.com>
2020-08-06 11:05:57 +0700

    Update follow

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_074547) do
+ActiveRecord::Schema.define(version: 2020_08_06_030857) do
 
   create_table "accounts", force: :cascade do |t|
     t.text "first_name"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_08_04_074547) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["account_id"], name: "index_albums_on_account_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "photos", force: :cascade do |t|
