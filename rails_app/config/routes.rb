3dfa80542ae92cee3424a14491a3560bed074a8a
Thanh Hai <thanhhai691999@gmail.com>
2020-08-08 17:35:58 +0700

    Add config route

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Tạo tài khoản
+  resources :signup, :login, only: :create
+  #Khi click vào sẽ update photo hoặc album được react
+  get 'reaction', to: 'albums#update', as: 'reaction_album'
+  get 'reaction', to: 'photo#update', as: "reaction_photo"  
+  #
+  resources :photos, :albums
+  #
+  resources :follows, only: [:index, :show, :update, :destroy]
+  #
+  resources :profiles, except: [:index,:create]
+  #
+  namespace :admin do
+    resources :signup, :login, only: :create
+    resources :photos, :albums
+    resources :profiles, except: [:index,:create]
+  end
 end
